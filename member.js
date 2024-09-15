08a5228e29cf2dd4c48babdb1a230b2fbdd60f26
iikkalord <59040108+iikkalord@users.noreply.github.com>
2024-09-15 17:08:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,120 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: 'HTML',
+            level: 7
+        },
+        {
+            name: 'CSS',
+            level: 7
+        },
+        {
+            name: 'Javascript',
+            level: 6
+        },
+        {
+            name: 'NodeJS',
+            level: 6
+        },
+        {
+            name: 'ReactJS',
+            level: 6
+        },
+        {
+            name: 'AngularJS',
+            level: 5
+        },
+        {
+            name: 'VueJS',
+            level: 4
+        },
+        {
+            name: 'PHP',
+            level: 6
+        },
+        {
+            name: 'Python',
+            level: 5
+        },
+        {
+            name: 'Java',
+            level: 4
+        },
+        {
+            name: 'C++',
+            level: 3
+        },
+        {
+            name: 'C#',
+            level: 3
+        },
+        {
+            name: 'Ruby',
+            level: 3
+        },
+        {
+            name: 'SQL',
+            level: 6
+        },
+        {
+            name: 'MongoDB',
+            level: 5
+        },
+        {
+            name: 'Firebase',
+            level: 5
+        },
+        {
+            name: 'Git',
+            level: 6
+        },
+        {
+            name: 'Adobe Illustrator',
+            level: 6
+        },
+        {
+            name: 'Adobe XD',
+            level: 6
+        },
+        {
+            name: 'Figma',
+            level: 6
+        },
+        {
+            name: 'Sketch',
+            level: 6
+        },
+        {
+            name: 'Photoshop',
+            level: 5
+        },
+        {
+            name: 'After Effects',
+            level: 5
+        },
+        {
+            name: 'Premiere Pro',
+            level: 5
+        },
+        {
+            name: 'Blender',
+            level: 4
+        },
+        {
+            name: 'Unity',
+            level: 4
+        },
+        {
+            name: 'Unreal Engine',
+            level: 4
+        },
+        {
+            name: 'Maya',
+            level: 4
+        },
+        {
+            name: '3DS Max',
+            level: 4
+        },
+        {
+            name: 'ZBrush',
